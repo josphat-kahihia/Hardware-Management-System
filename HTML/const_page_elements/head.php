1f276e6c6f197eb4cf9bdaecde45c1dd60f52772
Josphat Kahihia <josphatkahihia@gmail.com>
2023-02-13 14:24:27 +0300

    Changed HTML pages to use PHP

---
+++ HTML/const_page_elements/head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head>
+    <meta charset ="UTF-8">
+    <title> <?php echo $title;?> </title>
+    <link rel="stylesheet" href="../../css/Main.css">
+</head>
+<body>
+<?php require "header.html"; ?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+$title = "Homepage";
+require "/HTML/const_page_elements/head.php";
+?>
+    <h1>Welcome to JH, the leading website in all matters hardware</h1>
+    <p>
+        <b>Know what to build with? <a href="HTML/order.html">Order here</a></b>
+        <br>
+        Don't know where to start? Interested in learning the process of building, how you move from idea to actualization? 
+        You've come to the right place. Click <a href="HTML/blog.html">here</a>. <br><br>
+
+        <a href="HTML/form.html">Get to the form page here.</a>
+    </p>
+    
+<?php require "HTML/const_page_elements/footer.html"; ?>
