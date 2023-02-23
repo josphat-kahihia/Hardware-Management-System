d27ccb7b0e482f2e63446841df06b924eb9d584f
Josphat Kahihia <josphatkahihia@gmail.com>
2023-02-23 13:27:09 +0300

    Added comment in blog.php

--- includes/head.php
+++ includes/head.php
@@ -2,6 +2,6 @@
 <head>
     <meta charset ="UTF-8">
     <title> <?php echo $title;?> </title>
-    <link rel="stylesheet" href="Css/master.css">
+    <link rel="stylesheet" href="css/master.css">
 </head>
 <body>
